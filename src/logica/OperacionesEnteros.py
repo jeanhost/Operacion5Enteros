3d3cbbb1ca940a190ceb06cce2ffc4c89b91d15f
dgamarra <dgamarra@continental.edu.pe>
2023-05-17 21:00:37 -0500

    MCD de tres números (rojo)

--- src/logica/OperacionesEnteros.py
+++ src/logica/OperacionesEnteros.py
@@ -1,3 +1,14 @@
 class OperacionesEnteros:
+    def __init__(self, numeros):
+        self.__numeros = numeros
+
     def MCD(self, numero1, numero2):
+        temporal = 0
+        while numero2 != 0:
+            temporal = numero2
+            numero2 = numero1 % numero2
+            numero1 = temporal
+        return numero1
+
+    def calcularMCD(self):
         return None
--- test/PruebaOperacionesEnteros.py
+++ test/PruebaOperacionesEnteros.py
@@ -3,7 +3,7 @@ from src.logica.OperacionesEnteros import OperacionesEnteros
 
 class PruebaOperacionesEnteros(unittest.TestCase):
     def setUp(self):
-        self.operacion = OperacionesEnteros()
+        self.operacion = OperacionesEnteros([])
 
     def tearDown(self):
         self.operacion = None
@@ -16,6 +16,20 @@ class PruebaOperacionesEnteros(unittest.TestCase):
 
         # Do
         resultadoActual = self.operacion.MCD(numero1, numero2)
+
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
+
+    def test_MCD_tresNumerosPositivos_retornaMCD(self):
+        # Arrange
+        numero1 = 24679
+        numero2 = 20387
+        numero3 = 16169
+        resultadoEsperado = 37
+        operacion = OperacionesEnteros([numero1, numero2, numero3])
+
+        # Do
+        resultadoActual = operacion.calcularMCD()
 
         # Assert
         self.assertEqual(resultadoEsperado, resultadoActual)
