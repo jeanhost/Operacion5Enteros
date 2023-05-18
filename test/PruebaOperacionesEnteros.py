2e3163ef35e503e5efb39140f0b2fefec5c96d9f
dgamarra <dgamarra@continental.edu.pe>
2023-05-17 20:39:02 -0500

    MCD de dos números (rojo)

--- test/PruebaOperacionesEnteros.py
+++ test/PruebaOperacionesEnteros.py
@@ -1,5 +1,21 @@
 import unittest
+from src.logica.OperacionesEnteros import OperacionesEnteros
 
 class PruebaOperacionesEnteros(unittest.TestCase):
-    def test_MCD(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.operacion = OperacionesEnteros()
+
+    def tearDown(self):
+        self.operacion = None
+
+    def test_MCD_dosNumerosPositivos_retornaMCD(self):
+        # Arrange
+        numero1 = 18
+        numero2 = 24
+        resultadoEsperado = 6
+
+        # Do
+        resultadoActual = self.operacion.MCD(numero1, numero2)
+
+        # Assert
+        self.assertEqual(resultadoEsperado, resultadoActual)
