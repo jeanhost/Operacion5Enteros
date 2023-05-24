688b544ca393394a5e8bc3bcfc4522bb1bd09db6
jeanhost <128657013+jeanhost@users.noreply.github.com>
2023-05-24 13:56:59 -0500

    Update OperacionesEnteros.py

--- src/logica/OperacionesEnteros.py
+++ src/logica/OperacionesEnteros.py
@@ -2,6 +2,14 @@ class OperacionesEnteros:
     def __init__(self, numeros):
         self.__numeros = numeros
 
+    def calcularMCD(self):
+        if len(self.__numeros) < 2 :
+            raise FaltanParametros
+        elif len(self.__numeros) > 1 :
+            return self.MCDMasDosNumeros()
+        else:
+            return 0
+
     def MCD(self, numero1, numero2):
         temporal = 0
         while numero2 != 0:
@@ -10,5 +18,15 @@ class OperacionesEnteros:
             numero1 = temporal
         return numero1
 
-    def calcularMCD(self):
-        return None
+    def MCDMasDosNumeros(self):
+        for n in (self.__numeros):
+            if not isinstance(n, int):
+                raise ValueError
+
+        cantidadNumeros = len(self.__numeros)
+        mcd = self.MCD(self.__numeros[0], self.__numeros[1])
+        i = 0
+        while( i < (cantidadNumeros-2)):
+            mcd =  self.MCD(mcd, self.__numeros[i+2])
+            i = i+1
+        return mcd
