fa480c6005f4309c7539fdfeab58e1bf91415e3c
jeanhost <128657013+jeanhost@users.noreply.github.com>
2023-05-24 13:57:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,17 @@
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from src.Logica.OperacionesEnteros import OperacionesEnteros
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    cantidadNumeros = int(input("Ingrese la Cantidad de números: "))
+    numeros = []
+    for i in range(cantidadNumeros):
+        print(f"Ingrese Número {i+1:2}: ", end="", flush=True)
+        numeros.append(int(input("")))
+
+    print(f"Números Ingresados = {numeros}")
+    operacionesEnteros = OperacionesEnteros(numeros)
+    print(f"El MCD es = {operacionesEnteros.calcularMCD()}")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
