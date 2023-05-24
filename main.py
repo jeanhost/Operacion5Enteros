4a2ea752f56c408a6a1774accf6cc0be57b832b0
jeanhost <128657013+jeanhost@users.noreply.github.com>
2023-05-24 14:00:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from src.Logica.OperacionesEnteros import OperacionesEnteros
+from src.logica.OperacionesEnteros import OperacionesEnteros
 
 if __name__ == '__main__':
     cantidadNumeros = int(input("Ingrese la Cantidad de números: "))
